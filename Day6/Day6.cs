f8a0fd2e21585d22dfa1f99edff124fb6cc72f50
Jeremy Skinner <jeremy@o3solution.com>
2023-12-06 18:16:12 -0600

    Day 6

---
+++ Day6/Day6.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+using System.Diagnostics;
+using System.Diagnostics.Tracing;
+using System.Reflection;
+
+namespace AOC2023.Day5
+{
+    internal class Day6
+    {
+       
+        public static void Solve()
+        {
+
+            var races = new Dictionary<double, double>();
+            races.Add(7,9);
+            races.Add(15, 40); 
+            races.Add(30, 200);
+
+           
+
+            int? winResult = null;
+
+            foreach (var race in races)
+            {
+                var time = race.Key;
+                var recordDistance = race.Value;
+
+                var button = 0; //how long to hold the button
+
+                var winCount = 0;
+                for (var i = 0; i < time; i++)
+                {
+                    var distance = button * (time - i);
+                    var win = distance > recordDistance;
+                    if (win)
+                    {
+                        winCount++;
+                    }
+                    
+                    button++;
+                }
+
+                if (winResult == null)
+                {
+                    winResult = winCount;
+                }
+                else
+                {
+                    winResult *= winCount;
+                }
+            }
+
+            Console.WriteLine("Result: " + winResult);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,11 +6,11 @@ using AOC2023.Day3;
 using AOC2023.Day4;
 using AOC2023.Day5;
 
-//Day1.SolvePart1();
+//Day1.Solve();
 //Day1.SolvePart2();
 
 
-//Day2.SolvePart1();
+//Day2.Solve();
 
 //Day2.SolvePart2();
 
@@ -18,10 +18,13 @@ using AOC2023.Day5;
 //var day3 = new Day3();
 //day3.SolvePart1And2();
 
-//Day4.SolvePart1();
+//Day4.Solve();
 //Day4.SolvePart2();
-var day5 = new Day5();
-await day5.Solve();
+
+//var day5 = new Day5();
+//await day5.Solve();
+
+Day6.Solve();
 
 
 
